ef45355da9315fd6fe951403e1c87eace0902be4
ehsanasgari <asgari@berkeley.edu>
2021-02-23 11:27:39 +0100

    add livestream bot

---
+++ QNetbots/bots_access/livestream_bot.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import os
+os.path.dirname(os.path.abspath(__file__)+'/../../')
+
+from QNetbots.core_bot_api.bot import Bot
+from QNetbots.core_bot_api.mregex_handler import MRegexHandler
+import random
+import string
+
+class LiveStreamBot(Bot):
+    greetings = {'dear': 'کاربر عزیز', 'not-recognized':'پیام شما مفهوم نبود، لطفا از الگوهای زیر استفاده کنید:',
+                 'pattern':('@پخش#حجم '+'@پخش#نو ')}
+
+    def __init__(self, username, password, server):
+        super(LiveStreamBot, self).__init__(username, password, server)
+
+        # Add arabic regex handler
+        live_handler = MRegexHandler("@پخش", self.process_command)
+        self.add_handler(live_handler)
+
+    @staticmethod
+    def get_password(length=10):
+        # choose from all lowercase letter
+        letters = string.ascii_lowercase + '0123456789' + string.ascii_uppercases
+        password = ''.join(random.choice(letters) for i in range(length))
+        return password
+
+    def run(self):
+        self.start_polling()
+
+    def process_command(self, room, event):
+
+        command = event['content']['body'].split('#')[-1].strip()
+
+        try:
+            if command=='نو':
+                password = LiveStreamBot.get_password()
+                room.send_text(F"{event['sender']} {LiveStreamBot.greetings['dear']}: {password}")
+
+            else:
+                room.send_text(F"{event['sender']} {LiveStreamBot.greetings['dear']}: {LiveStreamBot.greetings['not-recognized']}"
+                       F"\n {LiveStreamBot.greetings['pattern']}")
+        except:
+            room.send_text(F"{event['sender']} {LiveStreamBot.greetings['dear']}: {LiveStreamBot.greetings['not-recognized']}"
+                           F"\n {LiveStreamBot.greetings['pattern']}")
+
+
+
+
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from QNetbots.bots_ai.arabicnlp_bot import ArabicBot
+from QNetbots.bots_access.livestream_bot import LiveStreamBot
 
 if __name__ == "__main__":
     arabbot = ArabicBot('arabibot','','')
     arabbot.run()
+
+    streambot = LiveStreamBot('streambot','','')
+    streambot.run()
     # Infinitely read stdin to stall main thread while the bot runs in other threads
     while True:
         input()
