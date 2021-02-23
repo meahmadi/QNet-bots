409a40a08947ea1434375ec92c6ccd3daa708fbb
Elementor <element@vps494514.ovh.net>
2021-02-23 20:33:24 +0100

    add livestream setup

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from QNetbots.bots_ai.arabicnlp_bot import ArabicBot
 from QNetbots.bots_access.livestream_bot import LiveStreamBot
 
 if __name__ == "__main__":
-    arabbot = ArabicBot('arabibot','','')
+    arabbot = ArabicBot('arabibot','dadashmeisam','https://quranic.network')
     arabbot.run()
 
-    streambot = LiveStreamBot('streambot','','')
+    streambot = LiveStreamBot('streambot','DaDa$hMeiS@m','https://quranic.network')
     streambot.run()
     # Infinitely read stdin to stall main thread while the bot runs in other threads
     while True:
